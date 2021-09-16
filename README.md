ab4466eace1db0e4b771c0a5bacd4142527ab68d
Jared Barnes <jaredbarn@gmail.com>
2021-09-15 19:54:57 -0500

    Updated JavaScript game, README.md, and added Top Ten to bottom of page.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# guessing-game-about-me
-A guessing game about me for Code Fellows Lab 201
+# Guessing game and bio for Code Fellows Lab 201
+
+## Guessing Game
+Before the page loads a prompt asks the user to input their name. A gretting message is displayed and the user is asked to respond with a yes or not to take a quiz. 
+* If the user enters "yes" an alert pops up telling the user to get ready for the quiz.
+* If the user enters "no" an alert pops up telling the user they have no choice.
+* If the user cancels, the prompt goes away and my biography page is displayed.
+  - Once the user responds with "yes" or "no", a series of 5 questions is asked.
+  - After each user "guess" an alert tells the user if the respons is correct or not.
+* The user must guess a number between 1 and 10.
+* The user is prompted a question about my favorite fish to catch.
+  - 6 attempts are allowed
+  - If user is unable to guess the answer an alert provides all answers to the user.
+* Once all questions have been answered an alert thanks the user, by name, for playing the game and a total number of correct responses is display to the user.
+* At the end of the game my biography page displays.
+
+## Added Top Ten Places to Visit
+  A quick list of top ten places you should visit that I highly recommend.
+
--- app.js
+++ app.js
@@ -18,6 +18,9 @@ if (welcomeMess.toLowerCase() == "yes" || welcomeMess.toLowerCase() == "y") {
     alert("You have no choice!");
 }
 
+let totalCorrect = 0;
+
+//first prompted question
 let firstQuest = prompt("Did I live in Ohio? Yes or No?");
 //control logic for first question
 if (firstQuest.toLowerCase() == "yes" || firstQuest.toLowerCase() == "y") {
@@ -26,12 +29,14 @@ if (firstQuest.toLowerCase() == "yes" || firstQuest.toLowerCase() == "y") {
     firstQuest.toLowerCase() == "no" ||
     firstQuest.toLowerCase() == "n"
 ) {
+    totalCorrect++;
     alert("Yes! Great job.");
 }
-//second question
+//second prompted question
 let secondQuest = prompt("Do I have a dog? Yes or No");
 //control logic for second question
 if (secondQuest.toLowerCase() == "yes" || secondQuest.toLowerCase() == "y") {
+    totalCorrect++;
     alert("Great job!");
 } else if (
     secondQuest.toLowerCase() == "no" ||
@@ -40,11 +45,13 @@ if (secondQuest.toLowerCase() == "yes" || secondQuest.toLowerCase() == "y") {
     alert("Sorry you missed it!");
 }
 
+//thrid prompted question
 let thirdQuest = prompt(
     "Did I gradguate from the University of Tennessee? Yes or No."
 );
 //control logic for third question
 if (thirdQuest.toLowerCase() == "yes" || thirdQuest.toLowerCase() == "y") {
+    totalCorrect++;
     alert("I sure did!");
 } else if (
     thirdQuest.toLowerCase() == "no" ||
@@ -56,6 +63,7 @@ if (thirdQuest.toLowerCase() == "yes" || thirdQuest.toLowerCase() == "y") {
 let fourthQuest = prompt("Do I live in Peru? Yes or No.");
 //control logic for fourth question
 if (fourthQuest.toLowerCase() == "yes" || fourthQuest.toLowerCase() == "y") {
+    totalCorrect++;
     alert("Yes I do!");
 } else if (
     fourthQuest.toLowerCase() == "no" ||
@@ -64,9 +72,11 @@ if (fourthQuest.toLowerCase() == "yes" || fourthQuest.toLowerCase() == "y") {
     alert("Sorry, that's not correct!");
 }
 
+//fifth question
 let fifthQuest = prompt("Am I married? Yes or No.");
-
+//control logic for fifth question
 if (fifthQuest.toLowerCase() == "yes" || fifthQuest.toLowerCase() == "y") {
+    totalCorrect++;
     alert("Yes I am!");
 } else if (
     fifthQuest.toLowerCase() == "no" ||
@@ -75,4 +85,69 @@ if (fifthQuest.toLowerCase() == "yes" || fifthQuest.toLowerCase() == "y") {
     alert("You missed again!");
 }
 
-let thankYou = alert("Thank you " + userName + " for playing my little game.");
+//Number guessing game
+let numberGuess = parseInt(prompt("Please guess a number between 1 and 10?"));
+//generate and store a random number for loop.
+let someNumber = Math.floor(Math.random() * 10);
+//console.log(someNumber);
+//turn counter
+let count = 1;
+
+while (count < 4) {
+    //console.log(someNumber);
+    if (numberGuess === someNumber) {
+        totalCorrect++;
+        alert("You got it!");
+        break;
+    } else if (numberGuess < someNumber) {
+        numberGuess = parseInt(prompt(`Try again! Your guess was to low.`));
+        count++;
+    } else if (numberGuess > someNumber) {
+        numberGuess = parseInt(prompt(`Try again! Your guess was to high.`));
+        count++;
+    }
+    if (count == 4) {
+        alert(`The answer was ${someNumber}.`);
+    }
+}
+
+//user array question guessing game
+let myFish = [
+    "brown trout",
+    "bone fish",
+    "cutthroat trout",
+    "brook trout",
+    "blue gill",
+    "bass",
+];
+
+let userAnswer = false;
+let userAttempt = 0;
+
+//use for loop to iterate over array each time the user provides a response
+while (userAnswer == false && userAttempt < 6) {
+    console.log(userAnswer == false && userAttempt < 6);
+    //prompt user provide a response.
+    let guessFish = prompt(
+        "Guess what species of fish I enjoy catching the most?"
+    );
+    for (let i = 0; i < myFish.length; i++) {
+        if (guessFish.toLowerCase() == myFish[i]) {
+            //console.log("not working");
+            totalCorrect++;
+            alert("Great guess!");
+            userAnswer = true;
+        }
+    }
+    userAttempt++;
+}
+
+if (userAnswer == false) {
+    alert(
+        "Great try but these are my favorite fish, brown trout, bone fish, cutthroat trout, brook trout, blue gill, and bass."
+    );
+}
+
+let thankYou = alert(
+    `Thank you ${userName} for playing my little game. You got ${totalCorrect} correct questions.`
+);
